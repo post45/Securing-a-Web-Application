e1284fd2b307ac06b4a54c4bbe2cbe8e31047759
post45 <bashkuresh92@yandex.ru>
2020-10-09 12:18:22 -0400

    first commit

---
+++ src/main/java/com/arthur/SecuringWebApplication/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.SecuringWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/SecuringWebApplication/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.SecuringWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
